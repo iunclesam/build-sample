b6a64875be801acbc658ceb4c3aa474b1b55bb44
15339033176@163.com <15339033176@163.com>
2020-11-14 10:18:42 +0800

    合并压缩js 编译less

---
+++ gulp_test/gulpfile.js
@@ -0,0 +1,33 @@
+// 引入gulp模块
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var less = require("gulp-less");
+var cleanCss = require("gulp-clean-css");
+
+// 注册任务
+// gulp.task('任务名', function () {
+//    // 配置任务的操作
+// });
+
+// 注册合并压缩js的任务
+gulp.task('js', function () {
+    return gulp.src('src/js/*.js') // 找到目标原文件，将数据读取到gulp的内存
+        .pipe(concat('build.js')) // 合并文件
+        .pipe(gulp.dest('dist/js/')) // 临时输出文件到本地
+        .pipe(uglify()) // 压缩文件
+        .pipe(rename({suffix: '.min'})) // 重命名
+        .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('less', function () {
+    return gulp.src('src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('src/css'))
+});
+
+// 注册默认任务
+gulp.task('default', function () {
+    
+});
---
+++ gulp_test/src/js/test2.js
@@ -0,0 +1,7 @@
+(function () {
+    var result = [1,2,3,4].map(function (item, index) {
+        return item + 20;
+    });
+
+    console.log(result);
+})();
