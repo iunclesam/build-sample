978d7d01639ffe9fbfddc408c60c8b0f2d72860c
15339033176@163.com <15339033176@163.com>
2020-11-15 11:51:59 +0800

    webpack 打包插件

--- webpack_test/webpack.config.js
+++ webpack_test/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); // node内置的模块用来设置路径的
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //自动生成html文件的插件
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //清除之前打包的文件
 
 module.exports = {
     entry: './src/js/entry.js', // 入口文件的位置
@@ -30,6 +32,10 @@ module.exports = {
         ]
     },
     devServer: {
-        contentBase: './dist'
-    }
+        // contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './index.html'}),
+        new CleanWebpackPlugin(['dist']),
+    ]
 };
